deae5cfb8a70c1a9cf6d11c9999a4ee58d6a8f12
ambouh <aphricanboi@gmail.com>
2017-05-15 22:57:01 -0400

    changed header to have all content loaded from navController

--- public/javascripts/common/navController.js
+++ public/javascripts/common/navController.js
@@ -14,19 +14,36 @@
     function NavController() {
         var vm = this;
 
-        vm.header = [
-            {
-                href: "#",
-                link: "Home"
-            },
-            {
-                href: "#",
-                link: "About"
-            },
-            {
-                href: "#",
-                link: "Camps"
-            },
-        ];
+        vm.header = {
+                logo:[
+                    {
+                        classNames: "hidden-xs hidden-sm visible-md visible-lg",
+                        pictureUrl: "images/logo.png",
+                    },
+                    {
+                        classNames: "hidden-lg hidden-xs hidden-md visible-sm",
+                        pictureUrl: "images/logo-md.png",
+                    },
+                    {
+                        classNames: "hidden-lg hidden-md visible-xs",
+                        pictureUrl: "images/logo-sm.png",
+                    },
+                ],
+
+                links:[
+                    {
+                        href: "#",
+                        link: "Home"
+                    },
+                    {
+                        href: "#",
+                        link: "About"
+                    },
+                    {
+                        href: "#",
+                        link: "Camps"
+                    }
+                ]
+    };
     }
 }());
